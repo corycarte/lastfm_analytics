93349d4c0d778e0ad989d6fbaa7e2afac0ea1f93
Cory Carte <7457451+corycarte@users.noreply.github.com>
2021-12-02 14:24:58 -0500

    Minor updates to API python file

--- Classes/LastFmApi.py
+++ Classes/LastFmApi.py
@@ -1,13 +1,32 @@
-import yaml
+import requests
+import json
+
+API_ROOT = "http://ws.audioscrobbler.com/2.0/"
 
 class LastFmApi:
 
     _user = None
     _api_key = None
 
-    def __init__(self, config_file: str):
-        print("Loading configuration file {filename}".format(filename=config_file))
-        
+    def __init__(self, api_key: str):
+        self._api_key = api_key
+
+    def set_user(self, user: str):
+        self._user = user
+
+    def pull_last_days_songs(self, days: int) -> dict:
+        if days < 1:
+            return {}
+
+        req_endpoint = API_ROOT + '?method=user.getrecenttracks&user={user_name}&api_key={key}&format=json'.format(user_name=self._user, key=self._api_key)
+        response = requests.get(req_endpoint)
+
+        if response.status_code == 200:
+            res = json.loads(response.text)
+            return res
+
+        print("Error retrieving data: Status code {code}".format(code=response.status_code))
+        return {}
 
     def debug(self):
         print("Using LastFM to view\n\tuser: {user}\n\twith API Key: {key}".format(user=self._user, key=self._api_key))
--- pull_last_fm.py
+++ pull_last_fm.py
@@ -1,10 +1,36 @@
 import os
 
-
+import yaml
 
 # Import local packages
 from Classes.LastFmApi import LastFmApi as LastFm
 
+def no_config_error():
+    print("config.yaml doesn't exist")
+
 if __name__ == '__main__':
-    last_fm = LastFm("config.yaml")
+    if not os.path.exists("config.yaml"):
+        no_config_error()
+        exit(1)
+
+    user = api_key = time_range = None
+
+    with open("config.yaml", "r") as stream:
+        try:
+            conf = yaml.safe_load(stream)
+            print(conf)
+            user = conf["user_name"]
+            api_key = conf["api_key"]
+            time_range = conf["default_time_range"]
+        except yaml.YAMLError as exc:
+            print(exc)
+
+    last_fm = LastFm(api_key)
+    last_fm.set_user(user)
+
+
+    print(last_fm.pull_last_days_songs(0))
+    print(last_fm.pull_last_days_songs(time_range))
+
+
     last_fm.debug()
